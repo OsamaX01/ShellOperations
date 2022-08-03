0d10494d2a42a5b4ccb16bc94e1ce6776cabe89b
OsamaX01 <--global>
2022-08-03 09:15:19 +0300

    completed some parts of the scripts

--- operations.sh
+++ operations.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 directory=$1
-operation_argument=$3
 operation_type=$2
+operation_argument=$3
 
-test() {
+get_existing_directory() {
 	while :
 	do
 		find . -type d -iname "${directory}" > dir.txt
 		if [ -s ./dir.txt ] ; then break ; fi
 		read -p "please enter an existing directory: " directory
 	done
+}
 
+get_operation_type() {
 	while :
 	do
 		if [[ "${operation_type}" != "ana" ]] && [[ "${operation_type}" != "del" ]] && [[ "${operation_type}" != "arr" ]]
@@ -21,6 +23,55 @@ test() {
 			break
 		fi
 	done
+
 }
 
-test
+get_valid_argument() {
+	case $operation_type in 
+		ana) 
+			if [ -z ${operation_argument} ] ; then read -p "please enter a pattern to search: " operation_argument ; fi
+			;;
+		del) 
+			number='^[0-9]+$'
+			while [ -z ${operation_argument} ] || ! [[ $operation_argument =~ $number ]]
+			do
+				read -p "please enter a number represinting biggest file size (only numbers allowed): " operation_argument 
+			done
+			;;
+		arr)
+			char='^[a-zA-Z]+$'
+			while [ -z ${operation_argument} ] || ! [[ $operation_argument =~ $char ]]
+			do
+				read -p "please enter type of file you want to arrange (only alphabatical characters allowed): " operation_argument 
+			done
+	esac
+}
+
+analyze() {
+	count=0
+	cd $directory
+	for file in *
+	do
+		tmp="`grep -c ${operation_argument} ./$file`" 
+		count=$(expr $count + $tmp)
+	done
+	echo "Number of occurences is ${count}"
+}
+
+operate() {
+	case $operation_type in 
+		ana) 
+			analyze 
+			;;
+		del) 
+			delete
+			;;
+		arr)
+			arrange
+	esac
+}
+
+get_existing_directory
+get_operation_type
+get_valid_argument
+operate
